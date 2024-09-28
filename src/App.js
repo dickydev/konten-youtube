8cad2383e22de9112a3f2c2b55866dc87459942b
ACN ACER <dickyfernando062@gmail.com>
2024-09-28 16:35:51 +0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ThemeProvider from './components/useContext/ThemeProvider'
 import Toolbar from './components/useContext/Toolbar'
 
 const App = () => {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
   return (
     <ThemeProvider>
       <div>
         <h1>Dicky Fernando Sitepu Project</h1>
+        <p>Ini adalah paragraf</p>
+        <button onClick={toggleTheme} style={{backgroundColor: 'red'}}>
+                Toggle Theme to {theme === 'light' ? 'dark' : 'light'}
+            </button>
         <Toolbar />
       </div>
     </ThemeProvider>
